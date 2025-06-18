ffbc7714f359f2cd2ce86ba849c17468fc6b0022
denchika02@yandex.ru <eholocation@gmail.com>
2025-06-18 10:31:42 +0300

    fixed connect test

--- pkg/plugin/driver_test.go
+++ pkg/plugin/driver_test.go
@@ -18,7 +18,7 @@ func TestConnect(t *testing.T) {
 	ydb := Ydb{}
 
 	t.Run("should not error when valid settings passed", func(t *testing.T) {
-		settings := backend.DataSourceInstanceSettings{JSONData: []byte(fmt.Sprintf(`{ "authKind": "%s", "endpoint": %s, "dbLocation": "%s"}`, authKind, endpoint, dbLocation))}
+		settings := backend.DataSourceInstanceSettings{JSONData: []byte(fmt.Sprintf(`{"authKind": "%s", "endpoint": "%s", "dbLocation": "%s"}`, authKind, endpoint, dbLocation))}
 		_, err := ydb.Connect(settings, json.RawMessage{})
 		assert.Equal(t, nil, err)
 	})
