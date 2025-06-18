7cf6ae70c166ff412fae6fad148d34bfbf657e78
denchika02@yandex.ru <eholocation@gmail.com>
2025-06-18 08:31:37 +0300

    added new converters

--- pkg/converters/converters.go
+++ pkg/converters/converters.go
@@ -1,7 +1,9 @@
 package converters
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/google/uuid"
 	"reflect"
 	"regexp"
 	"time"
@@ -11,9 +13,6 @@ import (
 	"github.com/shopspring/decimal"
 )
 
-var decimalMatch, _ = regexp.Compile(`^Decimal\(`)
-var optionalDecimalMatch, _ = regexp.Compile(`^Optional<Decimal\(`)
-
 type Converter struct {
 	scanType   reflect.Type
 	fieldType  data.FieldType
@@ -21,146 +20,338 @@ type Converter struct {
 	convert    func(in interface{}) (interface{}, error)
 }
 
+var matchRegexes = map[string]*regexp.Regexp{
+	"Decimal":           regexp.MustCompile(`^Decimal\(`),
+	"Json":              regexp.MustCompile(`^Json\(`),
+	"Uuid":              regexp.MustCompile(`^Uuid\(`),
+	"List":              regexp.MustCompile(`^List<.*>`),
+	"Dict":              regexp.MustCompile(`^Dict<.*>`),
+	"Set":               regexp.MustCompile(`^Set<.*>`),
+	"Tuple":             regexp.MustCompile(`^Tuple<.*>`),
+	"Struct":            regexp.MustCompile(`^Struct<.*>`),
+	"Variant":           regexp.MustCompile(`^Variant<.*>`),
+	"Enum":              regexp.MustCompile(`^Enum<.*>`),
+	"Optional<Decimal>": regexp.MustCompile(`^Optional<Decimal\(`),
+	"Optional<Json>":    regexp.MustCompile(`^Optional<Json\(`),
+	"Optional<Uuid>":    regexp.MustCompile(`^Optional<Uuid\(`),
+	"Optional<List>":    regexp.MustCompile(`^Optional<List\(`),
+	"Optional<Dict>":    regexp.MustCompile(`^Optional<Dict\(`),
+	"Optional<Set>":     regexp.MustCompile(`^Optional<Set\(`),
+	"Optional<Tuple>":   regexp.MustCompile(`^Optional<Tuple\(`),
+	"Optional<Struct>":  regexp.MustCompile(`^Optional<Struct\(`),
+	"Optional<Variant>": regexp.MustCompile(`^Optional<Variant\(`),
+	"Optional<Enum>":    regexp.MustCompile(`^Optional<Enum\(`),
+}
+
 var Converters = map[string]Converter{
 	"Bool": {
-		scanType:  reflect.PtrTo(reflect.TypeOf(true)),
+		scanType:  reflect.PointerTo(reflect.TypeOf(true)),
 		fieldType: data.FieldTypeBool,
 	},
 	"Double": {
-		scanType:  reflect.PtrTo(reflect.TypeOf(float64(0))),
+		scanType:  reflect.PointerTo(reflect.TypeOf(float64(0))),
 		fieldType: data.FieldTypeFloat64,
 	},
 	"Float": {
-		scanType:  reflect.PtrTo(reflect.TypeOf(float32(0))),
+		scanType:  reflect.PointerTo(reflect.TypeOf(float32(0))),
 		fieldType: data.FieldTypeFloat32,
 	},
 	"Int64": {
-		scanType:  reflect.PtrTo(reflect.TypeOf(int64(0))),
+		scanType:  reflect.PointerTo(reflect.TypeOf(int64(0))),
 		fieldType: data.FieldTypeInt64,
 	},
 	"Int32": {
-		scanType:  reflect.PtrTo(reflect.TypeOf(int32(0))),
+		scanType:  reflect.PointerTo(reflect.TypeOf(int32(0))),
 		fieldType: data.FieldTypeInt32,
 	},
 	"Int16": {
-		scanType:  reflect.PtrTo(reflect.TypeOf(int16(0))),
+		scanType:  reflect.PointerTo(reflect.TypeOf(int16(0))),
 		fieldType: data.FieldTypeInt16,
 	},
 	"Int8": {
-		scanType:  reflect.PtrTo(reflect.TypeOf(int8(0))),
+		scanType:  reflect.PointerTo(reflect.TypeOf(int8(0))),
 		fieldType: data.FieldTypeInt8,
 	},
 	"Uint64": {
-		scanType:  reflect.PtrTo(reflect.TypeOf(uint64(0))),
+		scanType:  reflect.PointerTo(reflect.TypeOf(uint64(0))),
 		fieldType: data.FieldTypeUint64,
 	},
-
 	"Uint32": {
-		scanType:  reflect.PtrTo(reflect.TypeOf(uint32(0))),
+		scanType:  reflect.PointerTo(reflect.TypeOf(uint32(0))),
 		fieldType: data.FieldTypeUint32,
 	},
 	"Uint16": {
-		scanType:  reflect.PtrTo(reflect.TypeOf(uint16(0))),
+		scanType:  reflect.PointerTo(reflect.TypeOf(uint16(0))),
 		fieldType: data.FieldTypeUint16,
 	},
 	"Uint8": {
-		scanType:  reflect.PtrTo(reflect.TypeOf(uint8(0))),
+		scanType:  reflect.PointerTo(reflect.TypeOf(uint8(0))),
 		fieldType: data.FieldTypeUint8,
 	},
 	"Decimal": {
-		fieldType:  data.FieldTypeFloat64,
-		scanType:   reflect.PtrTo(reflect.TypeOf(decimal.Decimal{})),
 		convert:    decimalConvert,
-		matchRegex: decimalMatch,
+		fieldType:  data.FieldTypeFloat64,
+		matchRegex: matchRegexes["Decimal"],
+		scanType:   reflect.PointerTo(reflect.TypeOf(decimal.Decimal{})),
+	},
+	"String": {
+		fieldType: data.FieldTypeString,
+		scanType:  reflect.PointerTo(reflect.TypeOf("")),
+	},
+	"Utf8": {
+		fieldType: data.FieldTypeString,
+		scanType:  reflect.PointerTo(reflect.TypeOf("")),
+	},
+	"Json": {
+		convert:    jsonConvert,
+		fieldType:  data.FieldTypeJSON,
+		matchRegex: matchRegexes["Json"],
+		scanType:   reflect.TypeOf((*interface{})(nil)).Elem(),
+	},
+	"Uuid": {
+		convert:    uuidConvert,
+		fieldType:  data.FieldTypeString,
+		matchRegex: matchRegexes["Uuid"],
+		scanType:   reflect.PointerTo(reflect.TypeOf("")),
 	},
 	"Date": {
-		fieldType:  data.FieldTypeTime,
-		scanType:   reflect.PtrTo(reflect.TypeOf(time.Time{})),
+		fieldType: data.FieldTypeTime,
+		scanType:  reflect.PointerTo(reflect.TypeOf(time.Time{})),
 	},
 	"Datetime": {
-		fieldType:  data.FieldTypeTime,
-		scanType:   reflect.PtrTo(reflect.TypeOf(time.Time{})),
+		fieldType: data.FieldTypeTime,
+		scanType:  reflect.PointerTo(reflect.TypeOf(time.Time{})),
 	},
 	"Timestamp": {
-		fieldType:  data.FieldTypeTime,
-		scanType:   reflect.PtrTo(reflect.TypeOf(time.Time{})),
+		fieldType: data.FieldTypeTime,
+		scanType:  reflect.PointerTo(reflect.TypeOf(time.Time{})),
 	},
 	"Interval": {
-		scanType:  reflect.PtrTo(reflect.TypeOf(int64(0))),
+		convert:   intervalConvert,
+		scanType:  reflect.PointerTo(reflect.TypeOf(int64(0))),
 		fieldType: data.FieldTypeInt64,
-		convert: intervalConvert,
 	},
-	"Optional<Interval>": {
-		scanType:  reflect.PtrTo(reflect.PtrTo(reflect.TypeOf(int64(0)))),
-		fieldType: data.FieldTypeNullableInt64,
-		convert: intervalNullConvert,
+	"SmallSerial": {
+		scanType:  reflect.PointerTo(reflect.TypeOf(int16(0))),
+		fieldType: data.FieldTypeInt16,
+	},
+	"Serial2": {
+		scanType:  reflect.PointerTo(reflect.TypeOf(int16(0))),
+		fieldType: data.FieldTypeInt16,
+	},
+	"Serial": {
+		scanType:  reflect.PointerTo(reflect.TypeOf(int32(0))),
+		fieldType: data.FieldTypeInt32,
+	},
+	"Serial4": {
+		scanType:  reflect.PointerTo(reflect.TypeOf(int32(0))),
+		fieldType: data.FieldTypeInt32,
+	},
+	"Serial8": {
+		scanType:  reflect.PointerTo(reflect.TypeOf(int64(0))),
+		fieldType: data.FieldTypeInt64,
+	},
+	"BigSerial": {
+		scanType:  reflect.PointerTo(reflect.TypeOf(int64(0))),
+		fieldType: data.FieldTypeInt64,
+	},
+	"List<>": {
+		convert:    jsonConvert,
+		fieldType:  data.FieldTypeJSON,
+		matchRegex: matchRegexes["List"],
+		scanType:   reflect.TypeOf((*interface{})(nil)).Elem(),
+	},
+	"Dict<>": {
+		convert:    jsonConvert,
+		fieldType:  data.FieldTypeJSON,
+		matchRegex: matchRegexes["Dict"],
+		scanType:   reflect.TypeOf((*interface{})(nil)).Elem(),
+	},
+	"Set<>": {
+		convert:    jsonConvert,
+		fieldType:  data.FieldTypeJSON,
+		matchRegex: matchRegexes["Set"],
+		scanType:   reflect.TypeOf((*interface{})(nil)).Elem(),
+	},
+	"Tuple<>": {
+		convert:    jsonConvert,
+		fieldType:  data.FieldTypeJSON,
+		matchRegex: matchRegexes["Tuple"],
+		scanType:   reflect.TypeOf((*interface{})(nil)).Elem(),
+	},
+	"Struct<>": {
+		convert:    jsonConvert,
+		fieldType:  data.FieldTypeJSON,
+		matchRegex: matchRegexes["Struct"],
+		scanType:   reflect.TypeOf((*interface{})(nil)).Elem(),
+	},
+	"Variant<>": {
+		convert:    jsonConvert,
+		fieldType:  data.FieldTypeJSON,
+		matchRegex: matchRegexes["Variant"],
+		scanType:   reflect.TypeOf((*interface{})(nil)).Elem(),
+	},
+	"Enum<>": {
+		convert:    jsonConvert,
+		fieldType:  data.FieldTypeJSON,
+		matchRegex: matchRegexes["Enum"],
+		scanType:   reflect.TypeOf((*interface{})(nil)).Elem(),
 	},
 	"Optional<Bool>": {
-		scanType:  reflect.PtrTo(reflect.PtrTo(reflect.TypeOf(true))),
+		scanType:  reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(true))),
 		fieldType: data.FieldTypeNullableBool,
 	},
 	"Optional<Double>": {
-		scanType:  reflect.PtrTo(reflect.PtrTo(reflect.TypeOf(float64(0)))),
+		scanType:  reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(float64(0)))),
 		fieldType: data.FieldTypeNullableFloat64,
 	},
 	"Optional<Float>": {
-		scanType:  reflect.PtrTo(reflect.PtrTo(reflect.TypeOf(float32(0)))),
+		scanType:  reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(float32(0)))),
 		fieldType: data.FieldTypeNullableFloat32,
 	},
 	"Optional<Int64>": {
-		scanType:  reflect.PtrTo(reflect.PtrTo(reflect.TypeOf(int64(0)))),
+		scanType:  reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(int64(0)))),
 		fieldType: data.FieldTypeNullableInt64,
 	},
 	"Optional<Int32>": {
-		scanType:  reflect.PtrTo(reflect.PtrTo(reflect.TypeOf(int32(0)))),
+		scanType:  reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(int32(0)))),
 		fieldType: data.FieldTypeNullableInt32,
 	},
 	"Optional<Int16>": {
-		scanType:  reflect.PtrTo(reflect.PtrTo(reflect.TypeOf(int16(0)))),
+		scanType:  reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(int16(0)))),
 		fieldType: data.FieldTypeNullableInt16,
 	},
 	"Optional<Int8>": {
-		scanType:  reflect.PtrTo(reflect.PtrTo(reflect.TypeOf(int8(0)))),
+		scanType:  reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(int8(0)))),
 		fieldType: data.FieldTypeNullableInt8,
 	},
 	"Optional<Uint64>": {
-		scanType:  reflect.PtrTo(reflect.PtrTo(reflect.TypeOf(uint64(0)))),
+		scanType:  reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(uint64(0)))),
 		fieldType: data.FieldTypeNullableUint64,
 	},
-
 	"Optional<Uint32>": {
-		scanType:  reflect.PtrTo(reflect.PtrTo(reflect.TypeOf(uint32(0)))),
+		scanType:  reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(uint32(0)))),
 		fieldType: data.FieldTypeNullableUint32,
 	},
 	"Optional<Uint16>": {
-		scanType:  reflect.PtrTo(reflect.PtrTo(reflect.TypeOf(uint16(0)))),
+		scanType:  reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(uint16(0)))),
 		fieldType: data.FieldTypeNullableUint16,
 	},
 	"Optional<Uint8>": {
-		scanType:  reflect.PtrTo(reflect.PtrTo(reflect.TypeOf(uint8(0)))),
+		scanType:  reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(uint8(0)))),
 		fieldType: data.FieldTypeNullableUint8,
 	},
 	"Optional<Decimal>": {
-		fieldType:  data.FieldTypeNullableFloat64,
-		scanType:   reflect.PtrTo(reflect.PtrTo(reflect.TypeOf(decimal.Decimal{}))),
 		convert:    decimalNullConvert,
-		matchRegex: optionalDecimalMatch,
+		fieldType:  data.FieldTypeNullableFloat64,
+		matchRegex: matchRegexes["Optional<Decimal>"],
+		scanType:   reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(decimal.Decimal{}))),
+	},
+	"Optional<String>": {
+		fieldType: data.FieldTypeNullableString,
+		scanType:  reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(""))),
+	},
+	"Optional<Utf8>": {
+		fieldType: data.FieldTypeNullableString,
+		scanType:  reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(""))),
+	},
+	"Optional<Json>": {
+		convert:    jsonConvert,
+		fieldType:  data.FieldTypeJSON,
+		matchRegex: matchRegexes["Optional<Json>"],
+		scanType:   reflect.PointerTo(reflect.TypeOf((*interface{})(nil)).Elem()),
+	},
+	"Optional<Uuid>": {
+		convert:    uuidNullConvert,
+		fieldType:  data.FieldTypeNullableString,
+		matchRegex: matchRegexes["Optional<Uuid>"],
+		scanType:   reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(""))),
 	},
 	"Optional<Date>": {
-		fieldType:  data.FieldTypeNullableTime,
-		scanType:   reflect.PtrTo(reflect.PtrTo(reflect.TypeOf(time.Time{}))),
+		fieldType: data.FieldTypeNullableTime,
+		scanType:  reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(time.Time{}))),
 	},
 	"Optional<Datetime>": {
-		fieldType:  data.FieldTypeNullableTime,
-		scanType:   reflect.PtrTo(reflect.PtrTo(reflect.TypeOf(time.Time{}))),
+		fieldType: data.FieldTypeNullableTime,
+		scanType:  reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(time.Time{}))),
 	},
 	"Optional<Timestamp>": {
-		fieldType:  data.FieldTypeNullableTime,
-		scanType:   reflect.PtrTo(reflect.PtrTo(reflect.TypeOf(time.Time{}))),
+		fieldType: data.FieldTypeNullableTime,
+		scanType:  reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(time.Time{}))),
+	},
+	"Optional<Interval>": {
+		convert:   intervalNullConvert,
+		scanType:  reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(int64(0)))),
+		fieldType: data.FieldTypeNullableInt64,
+	},
+	"Optional<SmallSerial>": {
+		scanType:  reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(int16(0)))),
+		fieldType: data.FieldTypeNullableInt16,
+	},
+	"Optional<Serial2>": {
+		scanType:  reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(int16(0)))),
+		fieldType: data.FieldTypeNullableInt16,
+	},
+	"Optional<Serial>": {
+		scanType:  reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(int32(0)))),
+		fieldType: data.FieldTypeNullableInt32,
+	},
+	"Optional<Serial4>": {
+		scanType:  reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(int32(0)))),
+		fieldType: data.FieldTypeNullableInt32,
+	},
+	"Optional<Serial8>": {
+		scanType:  reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(int64(0)))),
+		fieldType: data.FieldTypeNullableInt64,
+	},
+	"Optional<BigSerial>": {
+		scanType:  reflect.PointerTo(reflect.PointerTo(reflect.TypeOf(int64(0)))),
+		fieldType: data.FieldTypeNullableInt64,
+	},
+	"Optional<List>": {
+		convert:    jsonConvert,
+		fieldType:  data.FieldTypeJSON,
+		matchRegex: matchRegexes["Optional<List>"],
+		scanType:   reflect.PointerTo(reflect.TypeOf((*interface{})(nil)).Elem()),
+	},
+	"Optional<Dict>": {
+		convert:    jsonConvert,
+		fieldType:  data.FieldTypeJSON,
+		matchRegex: matchRegexes["Optional<Dict>"],
+		scanType:   reflect.PointerTo(reflect.TypeOf((*interface{})(nil)).Elem()),
+	},
+	"Optional<Set>": {
+		convert:    jsonConvert,
+		fieldType:  data.FieldTypeJSON,
+		matchRegex: matchRegexes["Optional<Set>"],
+		scanType:   reflect.PointerTo(reflect.TypeOf((*interface{})(nil)).Elem()),
+	},
+	"Optional<Tuple>": {
+		convert:    jsonConvert,
+		fieldType:  data.FieldTypeJSON,
+		matchRegex: matchRegexes["Optional<Tuple>"],
+		scanType:   reflect.PointerTo(reflect.TypeOf((*interface{})(nil)).Elem()),
+	},
+	"Optional<Struct>": {
+		convert:    jsonConvert,
+		fieldType:  data.FieldTypeJSON,
+		matchRegex: matchRegexes["Optional<Struct>"],
+		scanType:   reflect.PointerTo(reflect.TypeOf((*interface{})(nil)).Elem()),
+	},
+	"Optional<Variant>": {
+		convert:    jsonConvert,
+		fieldType:  data.FieldTypeJSON,
+		matchRegex: matchRegexes["Optional<Variant>"],
+		scanType:   reflect.PointerTo(reflect.TypeOf((*interface{})(nil)).Elem()),
+	},
+	"Optional<Enum>": {
+		convert:    jsonConvert,
+		fieldType:  data.FieldTypeJSON,
+		matchRegex: matchRegexes["Optional<Enum>"],
+		scanType:   reflect.PointerTo(reflect.TypeOf((*interface{})(nil)).Elem()),
 	},
 }
 
-var ComplexTypes = []string{"Map"}
 var YdbConverters = YDBConverters()
 
 func YDBConverters() []sqlutil.Converter {
@@ -202,6 +393,46 @@ func createConverter(name string, converter Converter) sqlutil.Converter {
 			ConverterFunc: convert,
 		},
 	}
+}
+
+func jsonConvert(in interface{}) (interface{}, error) {
+	if in == nil {
+		return (*string)(nil), nil
+	}
+	jBytes, err := json.Marshal(in)
+	if err != nil {
+		return nil, err
+	}
+
+	rawJSON := json.RawMessage(jBytes)
+	return &rawJSON, nil
+}
+
+func uuidConvert(in interface{}) (interface{}, error) {
+	if in == nil {
+		return (*string)(nil), nil
+	}
+	v, ok := in.(*uuid.UUID)
+	if !ok {
+		return nil, fmt.Errorf("invalid uuid - %v", in)
+	}
+	f := (*v).String()
+	return f, nil
+}
+
+func uuidNullConvert(in interface{}) (interface{}, error) {
+	if in == nil {
+		return (*string)(nil), nil
+	}
+	v, ok := in.(**uuid.UUID)
+	if !ok {
+		return nil, fmt.Errorf("invalid uuid - %v", in)
+	}
+	if *v == nil {
+		return (*string)(nil), nil
+	}
+	f := (**v).String()
+	return f, nil
 }
 
 func intervalConvert(in interface{}) (interface{}, error) {
